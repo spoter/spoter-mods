874b698ac18dcb456c2ee6149bd9563e691dec54
spoter <spoter@mail.ru>
2015-12-23 13:44:35 +0300

    mod_winter_camo to Torsus_SD : set winter camo to all vehicles in battle

---
+++ mod_winter_camo/mod_winter_camo.py
@@ -0,0 +1,13 @@
+﻿import BigWorld
+from VehicleAppearance import VehicleAppearance
+import items.vehicles
+
+def new_getCamouflageParams(self, vehicle):
+    camos = items.vehicles.g_cache.customization(vehicle.typeDescriptor.type.customizationNationID)['camouflages']
+    for ids in camos:
+        if vehicle.typeDescriptor.type.compactDescr in camos[ids]['tiling'] and 'Cracked_ice' in camos[ids]['name']:
+            return (ids, 999999999, 0)
+    return old_getCamouflageParams(self, vehicle)
+
+old_getCamouflageParams = VehicleAppearance._VehicleAppearance__getCamouflageParams      
+VehicleAppearance._VehicleAppearance__getCamouflageParams = new_getCamouflageParams
