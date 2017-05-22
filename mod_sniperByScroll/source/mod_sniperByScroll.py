13bb3f952bcab09f3c4fef3eacca5985e0a849b8
spoter <spoter@mail.ru>
2017-05-22 22:31:05 +0300

    sniperByScroll 2.00 (22-05-2017), by Kainenger, spoter, angelsoft for 0.9.18.0

---
+++ mod_sniperByScroll/source/mod_sniperByScroll.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import BattleReplay
+import BigWorld
+from AvatarInputHandler.aih_constants import CTRL_MODE_NAME, GUN_MARKER_TYPE
+from AvatarInputHandler.control_modes import ArcadeControlMode
+from gui.Scaleform.daapi.view.battle.shared.crosshair import gm_components as _components
+# noinspection PyProtectedMember
+from gui.Scaleform.daapi.view.battle.shared.crosshair.gm_factory import _OptionalMarkersFactory
+from gui.Scaleform.genConsts.GUN_MARKER_VIEW_CONSTANTS import GUN_MARKER_VIEW_CONSTANTS as _CONSTANTS
+from gui.battle_control import avatar_getter
+from gui.battle_control.battle_constants import CROSSHAIR_VIEW_ID as _VIEW_ID
+
+
+def hookedActivateAlternateMode(self, pos=None, bByScroll=False):
+    ownVehicle = BigWorld.entity(BigWorld.player().playerVehicleID)
+    if ownVehicle is not None and ownVehicle.isStarted and avatar_getter.isVehicleBarrelUnderWater() or BigWorld.player().isGunLocked:
+        return
+    elif self._aih.isSPG and bByScroll:
+        self._cam.update(0, 0, 0, False, False)
+        if BattleReplay.isPlaying() and BigWorld.player().isGunLocked:
+            mode = BattleReplay.g_replayCtrl.getControlMode()
+            pos = BattleReplay.g_replayCtrl.getGunMarkerPos()
+            equipmentID = BattleReplay.g_replayCtrl.getEquipmentId()
+        else:
+            mode = CTRL_MODE_NAME.SNIPER
+            equipmentID = None
+            if pos is None:
+                pos = self.camera.aimingSystem.getDesiredShotPoint()
+                if pos is None:
+                    pos = self._gunMarker.getPosition()
+        self._aih.onControlModeChanged(mode, preferredPos=pos, aimingMode=self._aimingMode, saveZoom=not bByScroll, equipmentID=equipmentID)
+        return
+    else:
+        return hookActivateAlternateMode(self, pos, bByScroll)
+
+
+# noinspection PyUnusedLocal
+def create(self, markersInfo, vehicleInfo, components=None):
+    dataProvider = markersInfo.serverMarkerDataProvider if markersInfo.isServerMarkerActivated else markersInfo.clientMarkerDataProvider
+    markerType = GUN_MARKER_TYPE.SERVER if markersInfo.isServerMarkerActivated else GUN_MARKER_TYPE.CLIENT
+    component = self._findComponent(markerType, dataProvider, components, _CONSTANTS.SNIPER_GUN_MARKER_NAME)
+    if component is None:
+        component = _components.DefaultGunMarkerComponent(markerType, _VIEW_ID.SNIPER, _CONSTANTS.SNIPER_GUN_MARKER_NAME, _CONSTANTS.GUN_MARKER_LINKAGE, dataProvider)
+    return component,
+
+# noinspection PyProtectedMember
+hookActivateAlternateMode = ArcadeControlMode._ArcadeControlMode__activateAlternateMode
+ArcadeControlMode._ArcadeControlMode__activateAlternateMode = hookedActivateAlternateMode
+_OptionalMarkersFactory.create = create
+
+
+print '[LOAD_MOD]:  [sniperByScroll 2.00 (22-05-2017), by Kainenger, spoter, angelsoft]'
