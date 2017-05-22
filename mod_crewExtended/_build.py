8c90729b6ddba1125e1a3c7db48871c8bfe67272
spoter <spoter@mail.ru>
2017-05-22 16:08:22 +0300

    crewExtended 5.00 to 0.9.18

---
+++ mod_crewExtended/_build.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+import codecs
+import datetime
+import glob
+import json
+import os
+import re
+import shutil
+import subprocess
+import base64
+
+CLIENT_VERSION = '0.9.18.0'
+NAME = 'spoter.crewExtended'
+ADD_LICENSE = True
+
+
+class Build(object):
+    OUT_PATH = '.out'
+    PYC_PATH = os.path.join(OUT_PATH, 'res', 'scripts', 'client', 'gui', 'mods')
+    BUILD_PATH = os.path.join('source')
+    VERSION = None
+    RELEASE = '%s.wotmod' % NAME
+    DATE = datetime.datetime.now().strftime("%Y-%m-%d")
+    CONFIG_NAME = None
+
+    def __init__(self):
+        self.clear()
+        if not os.path.exists('release'): subprocess.check_call(['powershell', 'mkdir', 'release'])
+        self.readVersion()
+        self.createFileDict()
+        self.packWotmod()
+        self.clear()
+        print 'created: %s v%s (%s) to %s' % (self.RELEASE, self.VERSION["version"], self.DATE, CLIENT_VERSION)
+
+    def clear(self):
+        try:
+            shutil.rmtree(self.OUT_PATH, True)
+        except OSError:
+            pass
+
+    def readVersion(self):
+        filePath = os.path.join(self.BUILD_PATH, 'VERSION')
+        with codecs.open(filePath, 'r', encoding='utf-8') as versionFile:
+            data = versionFile.read().decode('utf-8')
+            versionFile.close()
+        self.VERSION = json.loads(data)
+
+    def createFileDict(self):
+        version = '{:.2f}'.format(float(self.VERSION["version"]))
+        files = [(os.path.join(self.BUILD_PATH, self.VERSION["source"]), 'self.version = ', "'v%s (%s)'" % (version, self.DATE)),
+                 (os.path.join(self.BUILD_PATH, self.VERSION["source"]), 'self.version_id = ', re.sub('[.\s]', '', '%s' % version)),
+                 (os.path.join(self.BUILD_PATH, self.VERSION["meta"]), '<version>', '%s</version>' % version),
+                 (os.path.join(self.BUILD_PATH, self.VERSION["config"]), '"version": ', re.sub('[.\s]', '', '%s' % version))]
+        for path in glob.glob(os.path.join(self.BUILD_PATH, self.VERSION["i18n"], "*.json")):
+            files.append((path, '"version": ', re.sub('[.\s]', '', '%s' % version)))
+        for path in files:
+            self.updateFiles(*path)
+
+    def updateFiles(self, path, string, text):
+        with open(path, 'a+') as xfile:
+            data = xfile.readlines()
+            newData = []
+            for line in data:
+                if 'self.ids = ' in line:
+                    self.configName = re.split('self.ids = ', line)[1]
+                if string in line:
+                    newData.append('%s%s%s\n' % (re.split(string, line)[0], string, text))
+                    continue
+                newData.append(line)
+        xfile.close()
+        with open(path, 'w') as xfile:
+            xfile.writelines(newData)
+        xfile.close()
+
+    def packWotmod(self):
+        self.RELEASE = '%s_%s.wotmod' % (NAME, '{:.2f}'.format(float(self.VERSION["version"])))
+        subprocess.check_call(['powershell', 'mkdir', self.PYC_PATH])
+
+        py = '%s' % os.path.join(self.BUILD_PATH, self.VERSION["source"])
+        pyc = '%sc' % self.VERSION["source"]
+        ps = '%s\%s' % (os.path.realpath(self.OUT_PATH), 'create-7zip.ps1')
+        metaPath = '%s' % os.path.join(self.BUILD_PATH, os.path.dirname(self.VERSION["meta"]))
+        metaFile = os.path.basename(self.VERSION["meta"])
+
+        subprocess.check_call(['python', '-m', 'compileall', py])
+        subprocess.call('powershell robocopy %s %s %s /COPYALL /MOV' % (os.path.realpath(self.BUILD_PATH), os.path.realpath(self.PYC_PATH), pyc))
+        subprocess.call('powershell robocopy %s %s %s /COPYALL' % (os.path.realpath(metaPath), os.path.realpath(self.OUT_PATH), metaFile))
+        if self.VERSION["resources"]:
+            for directory in self.VERSION["resources"]:
+                if os.path.exists(os.path.join(self.BUILD_PATH, directory)):
+                    subprocess.call('powershell robocopy %s %s /COPYALL /E' % (os.path.realpath(os.path.join(self.BUILD_PATH, directory)), os.path.realpath(os.path.join(self.OUT_PATH, 'res', directory))))
+
+        with open(ps, 'w') as xfile:
+            xfile.write('function create-7zip([String] $aDirectory, [String] $aZipfile){ [string]$pathToZipExe = "C:\Program Files\\7-zip\\7z.exe"; [Array]$arguments = "a", "-tzip", "-ssw", "-mx0", "$aZipfile", "$aDirectory"; & $pathToZipExe $arguments; }\n'
+                        'create-7zip "%s"  "%s"\n'
+                        'create-7zip "%s"  "%s"\n' % (os.path.realpath(os.path.join(self.OUT_PATH, 'res')), os.path.realpath(os.path.join('release', self.RELEASE)),
+                                                      os.path.realpath(os.path.join(self.OUT_PATH, metaFile)), os.path.realpath(os.path.join('release', self.RELEASE))))
+            if ADD_LICENSE:
+                xfile.write('create-7zip "%s"  "%s"\n' % (self.createLicense(), os.path.realpath(os.path.join('release', self.RELEASE))))
+
+        xfile.close()
+        subprocess.call('powershell -executionpolicy bypass -command "& {Set-ExecutionPolicy AllSigned; %s; Set-ExecutionPolicy Undefined}"' % ps)
+
+    def createLicense(self):
+        b64 = "DQogICAgICAgIERPIFdIQVQgVEhFIEZVQ0sgWU9VIFdBTlQgVE8gUFVCTElDIExJQ0VOU0UgDQogICAgICAgICAgICAgICAgICAgIFZlcnNpb24gMiwgRGVjZW1iZXIgMjAwNCANCg0KIENvcHlyaWdodCAoQykgMjAwNCBTYW0gSG9jZXZhciA8c2FtQGhvY2V2YXIubmV0PiANCg0KIEV2ZXJ5b25lIGlzIHBlcm1pdHRlZCB0byBjb3B5IGFuZCBkaXN0cmlidXRlIHZlcmJhdGltIG9yIG1vZGlmaWVkIA0KIGNvcGllcyBvZiB0aGlzIGxpY2Vuc2UgZG9jdW1lbnQsIGFuZCBjaGFuZ2luZyBpdCBpcyBhbGxvd2VkIGFzIGxvbmcgDQogYXMgdGhlIG5hbWUgaXMgY2hhbmdlZC4gDQoNCiAgICAgICAgICAgIERPIFdIQVQgVEhFIEZVQ0sgWU9VIFdBTlQgVE8gUFVCTElDIExJQ0VOU0UgDQogICBURVJNUyBBTkQgQ09ORElUSU9OUyBGT1IgQ09QWUlORywgRElTVFJJQlVUSU9OIEFORCBNT0RJRklDQVRJT04gDQoNCiAgMC4gWW91IGp1c3QgRE8gV0hBVCBUSEUgRlVDSyBZT1UgV0FOVCBUTy4NCg=="
+        output_name = os.path.realpath(os.path.join(self.OUT_PATH, 'LICENSE'))
+        data = base64.b64decode(b64)
+        with open(output_name, "wb") as output_file:
+            output_file.write(data)
+        output_file.close()
+        return output_name
+
+
+build = Build()
---
+++ mod_crewExtended/source/mod_crewExtended.py
@@ -0,0 +1,243 @@
+﻿# -*- coding: utf-8 -*-
+import math
+
+from CurrentVehicle import g_currentVehicle
+from gui.Scaleform.daapi.view.meta.CrewMeta import CrewMeta
+from gui.shared.gui_items.dossier import TankmanDossier
+from items import tankmen
+# noinspection PyUnresolvedReferences
+from gui.mods.mod_mods_gui import g_gui, inject
+
+class Config(object):
+    def __init__(self):
+        self.ids = 'crewExtended'
+        self.version = 'v5.00 (2017-05-22)'
+        self.version_id = 500
+        self.author = 'by spoter'
+        self.data = {
+            'version'                : self.version_id,
+            'enabled'                : True,
+            'personalFileTotalXP'    : True,
+            'personalFileSkillXP'    : True,
+            'personalFileSkillBattle': True,
+            'currentCrewRankOnTop' : True,
+            'currentCrewRankBattles': True,
+            'currentCrewRoleBattles': False,
+            'currentCrewBattleIcon': True,
+            'currentColor' : 6, 
+            'colors': ['0000FF', 'A52A2B', 'D3691E', '6595EE', 'FCF5C8', '00FFFF', '28F09C', 'FFD700', '008000', 'ADFF2E', 'FF69B5', '00FF00', 'FFA500', 'FFC0CB', '800080', 'FF0000', '8378FC', 'DB0400', '80D639', 'FFE041', 'FFFF00', 'FA8072'],
+            'color_i18n': ['UI_menu_blue', 'UI_menu_brown', 'UI_menu_chocolate', 'UI_menu_cornflower_blue', 'UI_menu_cream', 'UI_menu_cyan', 'UI_menu_emerald', 'UI_menu_gold', 'UI_menu_green', 'UI_menu_green_yellow', 'UI_menu_hot_pink', 'UI_menu_lime', 'UI_menu_orange', 'UI_menu_pink', 'UI_menu_purple', 'UI_menu_red', 'UI_menu_wg_blur', 'UI_menu_wg_enemy', 'UI_menu_wg_friend', 'UI_menu_wg_squad', 'UI_menu_yellow', 'UI_menu_nice_red']
+        }
+        self.i18n = {
+            'version'                                   : self.version_id,
+            'UI_description'                            : 'Crew Extended',
+            'UI_setting_personalFile_label'             : 'Personal File :',
+            'UI_setting_personalFileTotalXP_text'       : 'Show total exp',
+            'UI_setting_personalFileTotalXP_tooltip'    : '{HEADER}Info:{/HEADER}{BODY}Show Total Experience to current tankman{/BODY}',
+            'UI_setting_personalFileSkillXP_text'       : 'Show exp to 100%',
+            'UI_setting_personalFileSkillXP_tooltip'    : '{HEADER}Info:{/HEADER}{BODY}Show Experience to current skill with format: to 1% (to 100%){/BODY}',
+            'UI_setting_personalFileSkillBattle_text'   : 'Show battles to 100%',
+            'UI_setting_personalFileSkillBattle_tooltip': '{HEADER}Info:{/HEADER}{BODY}Show Battles to current skill with format: 1 battle to 1% (100 battle to 100%){/BODY}',
+            'UI_setting_currentCrew_label': 'Current Crew :',
+            'UI_setting_currentColor_text'   : 'Change color',
+            'UI_setting_currentColor_tooltip': '',
+            'UI_setting_currentCrewRankOnTop_text'   : 'Show Rank on Top',
+            'UI_setting_currentCrewRankOnTop_tooltip': '{HEADER}Info:{/HEADER}{BODY}Default Role on top in current crew list in hangar, change it! :){/BODY}',
+            'UI_setting_currentCrewRankBattles_text'   : 'Show Battles in Rank',
+            'UI_setting_currentCrewRankBattles_tooltip': '{HEADER}Info:{/HEADER}{BODY}Show Battles to current skill in Rank field{/BODY}',
+            'UI_setting_currentCrewRoleBattles_text'   : 'Show Battles in Role',
+            'UI_setting_currentCrewRoleBattles_tooltip': '{HEADER}Info:{/HEADER}{BODY}Show Battles to current skill in Role field{/BODY}',
+            'UI_setting_currentCrewBattleIcon_text'   : 'Show Battle Icon',
+            'UI_setting_currentCrewBattleIcon_tooltip': '',
+            'UI_menu_blue'           : 'Blue',
+            'UI_menu_brown'          : 'Brown',
+            'UI_menu_chocolate'      : 'Chocolate',
+            'UI_menu_cornflower_blue': 'Cornflower Blue',
+            'UI_menu_cream'          : 'Cream',
+            'UI_menu_cyan'           : 'Cyan',
+            'UI_menu_emerald'        : 'Emerald',
+            'UI_menu_gold'           : 'Gold',
+            'UI_menu_green'          : 'Green',
+            'UI_menu_green_yellow'   : 'Green Yellow',
+            'UI_menu_hot_pink'       : 'Hot Pink',
+            'UI_menu_lime'           : 'Lime',
+            'UI_menu_orange'         : 'Orange',
+            'UI_menu_pink'           : 'Pink',
+            'UI_menu_purple'         : 'Purple',
+            'UI_menu_red'            : 'Red',
+            'UI_menu_wg_blur'        : 'WG Blur',
+            'UI_menu_wg_enemy'       : 'WG Enemy',
+            'UI_menu_wg_friend'      : 'WG Friend',
+            'UI_menu_wg_squad'       : 'WG Squad',
+            'UI_menu_yellow'         : 'Yellow',
+            'UI_menu_nice_red'       : 'Nice Red'
+        }
+        self.data, self.i18n = g_gui.register_data(self.ids, self.data, self.i18n)
+        g_gui.register(self.ids, self.template, self.data, self.apply)
+        print '[LOAD_MOD]:  [%s %s, %s]' % (self.ids, self.version, self.author)
+
+    @inject.log
+    def template(self):
+        return {
+            'modDisplayName' : self.i18n['UI_description'],
+            'settingsVersion': self.version_id,
+            'enabled'        : self.data['enabled'],
+            'column1'        : [{
+                'type'   : 'Label',
+                'text'   : self.i18n['UI_setting_currentCrew_label'],
+                'tooltip': '',
+            }, {
+                'type'   : 'CheckBox',
+                'text'   : self.i18n['UI_setting_currentCrewRankOnTop_text'],
+                'value'  : self.data['currentCrewRankOnTop'],
+                'tooltip': self.i18n['UI_setting_currentCrewRankOnTop_tooltip'],
+                'varName': 'currentCrewRankOnTop'
+            }, {
+                'type'   : 'CheckBox',
+                'text'   : self.i18n['UI_setting_currentCrewBattleIcon_text'],
+                'value'  : self.data['currentCrewBattleIcon'],
+                'tooltip': self.i18n['UI_setting_currentCrewBattleIcon_tooltip'],
+                'varName': 'currentCrewBattleIcon'
+            }, {
+                'type'   : 'CheckBox',
+                'text'   : self.i18n['UI_setting_currentCrewRankBattles_text'],
+                'value'  : self.data['currentCrewRankBattles'],
+                'tooltip': self.i18n['UI_setting_currentCrewRankBattles_tooltip'],
+                'varName': 'currentCrewRankBattles'
+            }, {
+                'type'   : 'CheckBox',
+                'text'   : self.i18n['UI_setting_currentCrewRoleBattles_text'],
+                'value'  : self.data['currentCrewRoleBattles'],
+                'tooltip': self.i18n['UI_setting_currentCrewRoleBattles_tooltip'],
+                'varName': 'currentCrewRoleBattles'
+            }, {
+                'type'        : 'Dropdown',
+                'text'        : self.i18n['UI_setting_currentColor_text'],
+                'tooltip'     : self.i18n['UI_setting_currentColor_tooltip'],
+                'itemRenderer': 'DropDownListItemRendererSound',
+                'options'     : self.generator_menu(),
+                'width'       : 200,
+                'value'       : self.data['currentColor'],
+                'varName'     : 'currentColor'
+            }],
+            'column2'        : [{
+                'type'   : 'Label',
+                'text'   : self.i18n['UI_setting_personalFile_label'],
+                'tooltip': '',
+            }, {
+                'type'   : 'CheckBox',
+                'text'   : self.i18n['UI_setting_personalFileTotalXP_text'],
+                'value'  : self.data['personalFileTotalXP'],
+                'tooltip': self.i18n['UI_setting_personalFileTotalXP_tooltip'],
+                'varName': 'personalFileTotalXP'
+            }, {
+                'type'   : 'CheckBox',
+                'text'   : self.i18n['UI_setting_personalFileSkillBattle_text'],
+                'value'  : self.data['personalFileSkillBattle'],
+                'tooltip': self.i18n['UI_setting_personalFileSkillBattle_tooltip'],
+                'varName': 'personalFileSkillBattle'
+            }, {
+                'type'   : 'CheckBox',
+                'text'   : self.i18n['UI_setting_personalFileSkillXP_text'],
+                'value'  : self.data['personalFileSkillXP'],
+                'tooltip': self.i18n['UI_setting_personalFileSkillXP_tooltip'],
+                'varName': 'personalFileSkillXP'
+            }]
+        }
+
+    @inject.log
+    def generator_menu(self):
+        res = []
+        for i in xrange(0, len(self.data['colors'])):
+            res.append({
+                'label': '<font color="#%s">%s</font>' % (self.data['colors'][i], self.i18n[self.data['color_i18n'][i]])
+            })
+        return res
+
+    @inject.log
+    def apply(self, settings):
+        self.data = g_gui.update_data(self.ids, settings)
+        g_gui.update(self.ids, self.template)
+
+@inject.log
+def getLastSkillBattlesLeft(self, tankman):
+    if not self.getBattlesCount():
+        result = None
+    else:
+        avgExp = self.getAvgXP()
+        newSkillReady = self.tmanDescr.roleLevel == tankmen.MAX_SKILL_LEVEL and (len(self.tmanDescr.skills) == 0 or self.tmanDescr.lastSkillLevel == tankmen.MAX_SKILL_LEVEL)
+        if avgExp and not newSkillReady:
+            result = max(1, int(math.ceil(tankman.getNextSkillXpCost() / avgExp)))
+        else:
+            result = 0
+    return result
+
+
+# noinspection PyProtectedMember
+@inject.log
+def changeStats(data, dossier, tankman):
+    for index in data:
+        if config.data['personalFileTotalXP'] and index['label'] == 'common':
+            index['stats'] += (dossier._TankmanDossier__packStat('xp', tankman.descriptor.totalXP()),)
+        if index['label'] == 'studying':
+            for ids in index['stats']:
+                if config.data['personalFileSkillXP'] and ids['name'] == 'nextSkillXPLeft':
+                    ids['value'] = ' '
+                    ids['premiumValue'] = '%s (%s)' % (ids['premiumValue'], dossier._TankmanDossier__formatValueForUI(tankman.getNextSkillXpCost()))
+                if config.data['personalFileSkillBattle'] and ids['name'] == 'nextSkillBattlesLeft':
+                    battles = dossier.getLastSkillBattlesLeft(tankman)
+                    ids['value'] = '%s (%s)' % (ids['value'], dossier._TankmanDossier__formatValueForUI(battles))
+                    ids['premiumValue'] = '%s (%s)' % (ids['premiumValue'], dossier._TankmanDossier__formatValueForUI(dossier._TankmanDossier__getBattlesLeftOnPremiumVehicle(battles)))
+    return data
+
+
+# noinspection PyProtectedMember
+@inject.log
+def changeTankman(data):
+    for tankmenData in data['tankmen']:
+        if tankmenData['tankmanID'] in g_currentVehicle.item.crewIndices:
+            if config.data['currentCrewRankOnTop']:
+                role, rank = (tankmenData['role'], tankmenData['rank'])
+                tankmenData['role'], tankmenData['rank'] = (rank, role)
+            if not hasattr(g_currentVehicle, 'itemsCache'):
+                from gui.shared import g_itemsCache
+                g_currentVehicle.itemsCache = g_itemsCache
+            tankman = g_currentVehicle.itemsCache.items.getTankman(tankmenData['tankmanID'])
+            dossier = g_currentVehicle.itemsCache.items.getTankmanDossier(tankmenData['tankmanID'])
+            battles = dossier.getLastSkillBattlesLeft(tankman)
+            text = ''
+            if battles:
+                if g_currentVehicle.item.isPremium:
+                    battles = dossier._TankmanDossier__getBattlesLeftOnPremiumVehicle(battles)
+                color = config.data['colors'][config.data['currentColor']]
+                text = '<font color="#%s">%s </font>' % (color, dossier._TankmanDossier__formatValueForUI(battles))
+                if config.data['currentCrewBattleIcon']:
+                    text += '<img align=\"top\" src=\"img://gui/maps//icons/library/BattleResultIcon-1.png\" height=\"14\" width=\"14\" vspace=\"-3\"/>'
+            if config.data['currentCrewRankBattles']:
+                tankmenData['rank'] = text + tankmenData['rank']
+            if config.data['currentCrewRoleBattles']:
+                tankmenData['role'] = text + tankmenData['role']
+    return data
+
+
+config = Config()
+TankmanDossier.getLastSkillBattlesLeft = getLastSkillBattlesLeft
+
+
+@inject.hook(TankmanDossier, 'getStats')
+@inject.log
+def getStats(func, *args):
+    if config.data['enabled']:
+        return changeStats(func(*args), *args)
+    return func(*args)
+
+
+@inject.hook(CrewMeta, 'as_tankmenResponseS')
+@inject.log
+def tankmanResponse(func, *args):
+    if config.data['enabled']:
+        return func(args[0], changeTankman(args[1]))
+    return func(*args)
+
+
+
