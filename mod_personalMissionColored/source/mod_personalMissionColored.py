882fe0a666497a12a10092fb78f887cd00010b27
spoter <spoter@mail.ru>
2017-11-26 16:57:08 +0300

    mod_personalMissionColored 1.03 оптимизировал код :)

--- mod_personalMissionColored/source/mod_personalMissionColored.py
+++ mod_personalMissionColored/source/mod_personalMissionColored.py
@@ -1,14 +1,3 @@
-# -*- coding: utf-8 -*-
-from gui.Scaleform.daapi.view.lobby.missions.personal.personal_missions_map_view import PersonalMissionsMapView
 from gui.Scaleform.genConsts.PERSONAL_MISSIONS_ALIASES import PERSONAL_MISSIONS_ALIASES
-
-def hookedGetQuestStatusData(self, vehType, quest):
-    data = hookGetQuestStatusData(self, vehType, quest)
-    if data['state'] == PERSONAL_MISSIONS_ALIASES.REGION_COMPLETED:
-        data['state'] = 5
-    return data
-
-hookGetQuestStatusData = PersonalMissionsMapView._PersonalMissionsMapView__getQuestStatusData
-PersonalMissionsMapView._PersonalMissionsMapView__getQuestStatusData = hookedGetQuestStatusData
-
-print '[LOAD_MOD]:  [mod_personalMissionColored 1.02 (26-11-2017), by spoter]'
+PERSONAL_MISSIONS_ALIASES.REGION_COMPLETED = 5
+print '[LOAD_MOD]:  [mod_personalMissionColored 1.03 (26-11-2017), by spoter]'
