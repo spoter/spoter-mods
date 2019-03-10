b5c5efb0cb3c73dd285bd59bba4164a998d95110
spoter <spoter@mail.ru>
2019-03-10 14:55:15 +0300

    mod_tooltipsCountItemsLimitExtend 1.01 На складе показывает полную цену продажи модулей, расходников и боеприпасов.

--- mod_tooltipsCountItemsLimitExtend/source/mod_tooltipsCountItemsLimitExtend.py
+++ mod_tooltipsCountItemsLimitExtend/source/mod_tooltipsCountItemsLimitExtend.py
@@ -1,4 +1,24 @@
 # -*- coding: utf-8 -*-
 from gui.shared.tooltips.module import ModuleTooltipBlockConstructor
+from gui.Scaleform.daapi.view.lobby.storage import storage_helpers
+
+def createStorageDefVO(itemID, title, description, count, price, image, imageAlt, itemType='', nationFlagIcon='', enabled=True, contextMenuId=''):
+    b = []
+    for priced in price['price']:
+        b.append((priced[0], priced[1] * count))
+    price['price'] = tuple(b)
+    return {'id': itemID,
+     'title': title,
+     'description': description,
+     'count': count,
+     'price': price,
+     'image': image,
+     'imageAlt': imageAlt,
+     'type': itemType,
+     'nationFlagIcon': nationFlagIcon,
+     'enabled': enabled,
+     'contextMenuId': contextMenuId}
+storage_helpers.createStorageDefVO = createStorageDefVO
 ModuleTooltipBlockConstructor.MAX_INSTALLED_LIST_LEN = 1000
-print '[LOAD_MOD]:  [mod_tooltipsCountItemsLimitExtend 1.00 (11-05-2018), by spoter, gox]'
+
+print '[LOAD_MOD]:  [mod_tooltipsCountItemsLimitExtend 1.01 (10-03-2019), by spoter, gox]'
