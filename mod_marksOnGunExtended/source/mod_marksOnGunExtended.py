fabdee456dee945c60e33e0dbcd7005b3fe0ccf2
spoter <spoter@mail.ru>
2019-10-12 00:08:45 +0300

    fix errors in techTree flash

--- mod_marksOnGunExtended/source/mod_marksOnGunExtended.py
+++ mod_marksOnGunExtended/source/mod_marksOnGunExtended.py
@@ -71,8 +71,8 @@ techTreeWidth = 54
 class Config(object):
     def __init__(self):
         self.ids = 'marksOnGunExtended'
-        self.version = 'v8.04 (2019-10-08)'
-        self.version_id = 804
+        self.version = 'v8.05 (2019-10-11)'
+        self.version_id = 805
         self.author = 'by spoter to b4it.org & pfmods.net'
         self.buttons = {
             'buttonShow'    : [Keys.KEY_NUMPAD9, [Keys.KEY_LALT, Keys.KEY_RALT]],
