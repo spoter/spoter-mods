1d74396ae1f96cdcb5396aa01d7e6391698aec65
spoter <spoter@mail.ru>
2020-03-05 12:53:14 +0300

    fix 1.8.0

--- mod_tooltipsCountItemsLimitExtend/source/mod_tooltipsCountItemsLimitExtend.py
+++ mod_tooltipsCountItemsLimitExtend/source/mod_tooltipsCountItemsLimitExtend.py
@@ -2,26 +2,19 @@
 from gui.shared.tooltips.module import ModuleTooltipBlockConstructor
 from gui.shared.tooltips import battle_booster
 from gui.Scaleform.daapi.view.lobby.storage import storage_helpers
+from goodies.goodie_constants import GOODIE_STATE
 
+oldCreateStorageDefVO = storage_helpers.createStorageDefVO
 
-def createStorageDefVO(itemID, title, description, count, price, image, imageAlt, itemType='', nationFlagIcon='', enabled=True, contextMenuId=''):
+def createStorageDefVO(itemID, title, description, count, price, image, imageAlt, itemType='', nationFlagIcon='', enabled=True, available=True, contextMenuId='', additionalInfo='', active=GOODIE_STATE.INACTIVE, upgradable=False, upgradeButtonTooltip=''):
+    result = oldCreateStorageDefVO(itemID, title, description, count, price, image, imageAlt, itemType, nationFlagIcon, enabled, available, contextMenuId, additionalInfo, active, upgradable, upgradeButtonTooltip)
     b = []
-    for priced in price['price']:
-        b.append((priced[0], priced[1] * count))
-    price['price'] = tuple(b)
-    return {'id': itemID,
-     'title': title,
-     'description': description,
-     'count': count,
-     'price': price,
-     'image': image,
-     'imageAlt': imageAlt,
-     'type': itemType,
-     'nationFlagIcon': nationFlagIcon,
-     'enabled': enabled,
-     'contextMenuId': contextMenuId}
+    for priced in result['price']['price']:
+        b.append((priced[0], priced[1] * result['count']))
+    result['price']['price'] = tuple(b)
+    return result
 storage_helpers.createStorageDefVO = createStorageDefVO
 ModuleTooltipBlockConstructor.MAX_INSTALLED_LIST_LEN = 1000
 battle_booster._MAX_INSTALLED_LIST_LEN = 1000
 
-print '[LOAD_MOD]:  [mod_tooltipsCountItemsLimitExtend 1.02 (18-08-2019), by spoter, gox]'
+print '[LOAD_MOD]:  [mod_tooltipsCountItemsLimitExtend 1.03 (05-03-2020), by spoter, gox]'
