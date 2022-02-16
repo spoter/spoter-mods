2ceabe42b24b872eafa527a8f0e60fc61e15df30
Peter Rastorguev <spoter@mail.ru>
2022-02-17 01:00:31 +0300

    muteSoundHotKey 1.02 фикс ошибок, добавлена опция минимального звука

--- mod_muteSoundHotKey/source/mod_muteSoundHotKey.py
+++ mod_muteSoundHotKey/source/mod_muteSoundHotKey.py
@@ -14,14 +14,15 @@ from helpers import getLanguageCode
 class _Config(object):
     def __init__(self):
         self.ids = 'muteSoundHotKey'
-        self.version = 'v1.01 (2022-02-16)'
-        self.version_id = 101
+        self.version = 'v1.02 (2022-02-17)'
+        self.version_id = 102
         self.author = 'by spoter'
         self.dataDefault = {
             'version'                   : self.version_id,
             'enabled'                   : True,
             'showMessage'               : True,
             'restoreSoundAfterBattleEnd': True,
+            'mutedSoundVolume' : 0.0,
             'hotkey'                    : [Keys.KEY_F9]
         }
         self.i18n = {
@@ -32,15 +33,19 @@ class _Config(object):
             'UI_setting_restoreSoundAfterBattleEnd_text': 'Restore Sound after battle end',
             'UI_setting_soundMuted'                     : 'Game Sound ON',
             'UI_setting_soundRestored'                  : 'Game Sound OFF',
+            'UI_setting_mutedSoundVolume': 'Minimal sound level when muted',
+            'UI_setting_mutedSoundVolume_formats' : '%'
         }
         if 'ru' in '%s'.lower() % getLanguageCode():
             self.i18n.update({
-                'UI_description'                            : 'Горячая клавиша отключения звуков в игре',
+                'UI_description'                            : 'Горячая клавиша заглушения звуков в игре',
                 'UI_setting_hotkey_text'                    : 'Горячая клавиша',
                 'UI_setting_showMessage_text'               : 'Показывать сообщение в бою',
                 'UI_setting_restoreSoundAfterBattleEnd_text': 'Восстанавливать звук после окончания текущего боя',
                 'UI_setting_soundMuted'                     : 'Игровые Звуки ВКЛ',
                 'UI_setting_soundRestored'                  : 'Игровые Звуки ВЫКЛ',
+                'UI_setting_mutedSoundVolume_text'        : 'Минимальный уровень звука, когда заглушено',
+                'UI_setting_mutedSoundVolume_formats': '%'
             })
         self.data, self.i18n = g_gui.register_data(self.ids, self.dataDefault, self.i18n, 'spoter')
         g_gui.register(self.ids, self.template, self.data, self.apply)
@@ -57,6 +62,7 @@ class _Config(object):
             ],
             'column2'        : [
                 g_gui.optionButton(*self.p__getI18nParamButton('hotkey', 'F9')),
+                g_gui.optionSlider(*self.p__getI18nParamSlider('mutedSoundVolume', 0, 100, 1)),
             ]
         }
 
@@ -71,6 +77,13 @@ class _Config(object):
     def p__getI18nParamButton(self, name, defaultValueText):
         params = self.p__getI18nParam(name)
         return params[:5] + (defaultValueText,)
+
+    def p__getI18nParamSlider(self, name, minValue, maxValue, step):
+        # return varName, value, defaultValue, minValue, maxValue, step, text, formats, tooltip, defaultValueText
+        params = self.p__getI18nParam(name)
+        formats = 'UI_setting_%s_formats' % name
+        formats = self.i18n[formats] if formats in self.i18n else ''
+        return params[0], params[1], params[2], minValue, maxValue, step, params[3], formats, params[4], params[5]
 
     def apply(self, settings):
         self.data = g_gui.update_data(self.ids, settings, 'spoter')
@@ -88,7 +101,8 @@ class Support(object):
     def endBattle(self):
         InputHandler.g_instance.onKeyDown -= self.injectButton
         if config.data['enabled'] and config.data['restoreSoundAfterBattleEnd'] and self.mutedSound:
-            BigWorld.player().gunRotator.settingsCore.applySetting(settings_constants.SOUND.MASTER, self.masterVolume)
+            BigWorld.player().settingsCore.applySetting(settings_constants.SOUND.MASTER, self.masterVolume)
+            self.mutedSound = False
 
     def showStatusMessage(self):
         if config.data['enabled'] and config.data['showMessage']:
@@ -103,10 +117,10 @@ class Support(object):
             if not player or not config.data['enabled']:
                 return
             if not self.mutedSound:
-                self.masterVolume = player.gunRotator.settingsCore.getSetting(settings_constants.SOUND.MASTER)
-                player.gunRotator.settingsCore.applySetting(settings_constants.SOUND.MASTER, 0.0)
+                self.masterVolume = player.settingsCore.getSetting(settings_constants.SOUND.MASTER)
+                player.settingsCore.applySetting(settings_constants.SOUND.MASTER, config.data['mutedSoundVolume'])
             else:
-                player.gunRotator.settingsCore.applySetting(settings_constants.SOUND.MASTER, self.masterVolume)
+                player.settingsCore.applySetting(settings_constants.SOUND.MASTER, self.masterVolume)
             self.showStatusMessage()
             self.mutedSound = not self.mutedSound
 
