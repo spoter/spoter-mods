7987aa5401e20e594b416174110ff977484a1df7
spoter <spoter@mail.ru>
2025-02-27 08:36:19 +0300

    проверка сборки #23 "релиз всех модов"

--- .github/createRelease.py
+++ .github/createRelease.py
@@ -2,17 +2,18 @@
 import traceback
 import os
 import argparse
+import json
 
 parser = argparse.ArgumentParser(description='Create mod archives.')
 parser.add_argument('lesta_version', help='Client version for Lesta')
 parser.add_argument('wg_version', help='Client version for WG')
-parser.add_argument('updated_mods', help='Mods lists in format ["name1", "name2"] or []')
+parser.add_argument('updated_mods', help='Mods lists in format "name1,name2" or ""')
 
 args = parser.parse_args()
 
 CLIENT_VERSION_WG = args.wg_version    # Международная версия клиента
 CLIENT_VERSION_RU = args.lesta_version # Леста версия клиента
-updated_mods = args.mods_list          # список модов для обновления
+updated_mods = args.updated_mods.split(",") if args.updated_mods else []  # список модов для обновления
 
 def test_main_folder():
     main_folder = os.path.realpath('./../mod_mods_gui')
@@ -24,11 +25,11 @@ def test_main_folder():
 
 MAIN_FOLDER = test_main_folder()
 
-
 print('---------------------')
 print('Start building')
 print('      RU: {}'.format(CLIENT_VERSION_RU))
 print('      WG: {}'.format(CLIENT_VERSION_WG))
+print('      Mods: {}'.format(updated_mods))
 
 script_path = os.path.realpath(os.path.join(MAIN_FOLDER, '.github/builder.py'))
 for MOD_NAME in updated_mods:
@@ -38,7 +39,6 @@ for MOD_NAME in updated_mods:
         print('ERROR: {}'.format(e))
         traceback.print_exc()
         break
-
 
 print('---------------------')
 print('DONE')
