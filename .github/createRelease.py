ff104127466e877535299239336d231a785be929
spoter <spoter@mail.ru>
2025-02-27 03:07:36 +0300

    обновление автосборки только для нужного мода и для всех модов, проверочная фраза для этого пуша "релиз всех модов"

--- .github/createRelease.py
+++ .github/createRelease.py
@@ -7,11 +7,13 @@ import argparse
 parser = argparse.ArgumentParser(description='Create mod archives.')
 parser.add_argument('lesta_version', help='Client version for Lesta')
 parser.add_argument('wg_version', help='Client version for WG')
+parser.add_argument('updated_mods', help='Mods lists in format ["name1", "name2"] or []')
 
 args = parser.parse_args()
 
 CLIENT_VERSION_WG = args.wg_version    # Международная версия клиента
 CLIENT_VERSION_RU = args.lesta_version # Леста версия клиента
+updated_mods = args.mods_list          # список модов для обновления
 
 def test_main_folder():
     main_folder = os.path.realpath('./../mod_mods_gui')
@@ -23,27 +25,14 @@ def test_main_folder():
 
 MAIN_FOLDER = test_main_folder()
 
-mods_list = [
-    'mod_mods_gui',
-    'mod_artySplash',
-    'mod_autoServerSight',
-    'mod_creditCalc',
-    'mod_crewExtended',
-    'mod_discordHangar',
-    'mod_marksOnGunExtended',
-    'mod_muteSoundHotKey',
-    'mod_repair_extended',
-    'mod_restartRandomQueue',
-    'mod_server_turret_extended',
-    'mod_spotted_extended_light',
-]
+
 print('---------------------')
 print('Start building')
 print('      RU: {}'.format(CLIENT_VERSION_RU))
 print('      WG: {}'.format(CLIENT_VERSION_WG))
 
 script_path = os.path.realpath(os.path.join(MAIN_FOLDER, '.github/builder.py'))
-for MOD_NAME in mods_list:
+for MOD_NAME in updated_mods:
     try:
         os.system("python {} {} {} {}".format(script_path, MOD_NAME, CLIENT_VERSION_RU, CLIENT_VERSION_WG))
     except Exception as e:
